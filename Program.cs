5f2e7a213ffc1d22fbaa0f4f8df8d1d2e3593cc3
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 20:07:34 -0600

    Programa Creado!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//Escriba un programa que cuente cuántos números hay del 1 al 50 utilizando do-while.
