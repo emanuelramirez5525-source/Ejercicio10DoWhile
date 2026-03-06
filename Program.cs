aeb699eb219de2d5b14fba6070a9cf09dd29afaa
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 20:36:09 -0600

    Logica Aplicada!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//Escriba un programa que cuente cuántos números hay del 1 al 50 utilizando do-while.
+﻿//Escriba un programa que cuente cuántos números hay del 1 al 50 utilizando do-while.
+
+
+int num = 1;
+int contador = 0;
+
+do
+{
+    num++; contador++;
+   
+} while (num <= 50);
+
+Console.WriteLine("La cantidad de números es = "+ contador);
+
